3c0bcdecae828bcce82aafae1892b9eb27d8cf68
111092 <111092@SMC-LL-126-19>
2023-02-02 15:43:48 +0100

    Add project files.

---
+++ rrwebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace rrwebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
